76235a2444d98d660e782ae81bcb109c71220b71
Gabriel Casemiro <gabrielcasemiro68@gmail.com>
2024-12-05 20:03:23 -0300

    Implementing analytics service with claims and results & updating tests

--- src/services/analytics.py
+++ src/services/analytics.py
@@ -1,7 +1,7 @@
 import logging
 from ..models.claim import Claim
 from ..models.revert import Revert
-from analytics_interface import AnalyticsInterface
+from .analytics_interface import AnalyticsInterface
 from typing import List
 import json
 
@@ -13,23 +13,59 @@ class Analytics(AnalyticsInterface):
     def compute_metrics(self, claims: List[Claim], reverts: List[Revert]):
         results = []
         data = {}
+        claims_by_id = {}
 
         # compute claims
-        results.append(
-            {
-                "npi": "1234",
-                "ndc": "12345",
-                "fills": 1,
-                "reverted": 2,
-                "avg_price": 123.4,
-                "total_price": 123.4,
-            }
-        )
-        logging.info(
-            "Claims compute before reverts: %s" % json.dumps(results, indent=2)
-        )
+        for claim in claims:
+            if claim.npi not in self.allowed_npis:
+                logging.info(
+                    f"Ignored claim {claim.npi} because it's not included in the allowed npis list"
+                )
+                continue
 
-        # compute reverts
-        logging.info("Claims compute after reverts: %s" % json.dumps(results, indent=2))
+            if claim.id in claims_by_id.keys():
+                logging.info(f"Ignored claim {claim.npi} because it's duplicated")
+                continue
+
+            key = (claim.npi, claim.ndc)
+
+            claims_by_id[claim.id] = {
+                "key": key,
+                "price": claim.price,
+                "quantity": claim.quantity,
+            }
+            if key in data.keys():
+                data[key]["fills"] += 1
+                data[key]["total_price"] += claim.price
+                data[key]["total_quantity"] += claim.quantity
+            else:
+                data[key] = {
+                    "fills": 1,
+                    "reverted": 0,
+                    "total_price": claim.price,
+                    "total_quantity": claim.quantity,
+                }
+
+        # logging.info("Claims compute before reverts: %s" % data.values())
+
+        # # compute reverts
+        # logging.info("Claims compute after reverts: %s" % data)
+
+        for key_data, value in data.items():
+            if value["total_quantity"] > 0:
+                avg_price = value["total_price"] / value["total_quantity"]
+            else:
+                avg_price = 0.0
+
+            results.append(
+                {
+                    "npi": key_data[0],  # npi
+                    "ndc": key_data[1],  # ndc
+                    "fills": value["fills"],
+                    "reverted": value["reverted"],
+                    "avg_price": avg_price,
+                    "total_price": value["total_price"],
+                }
+            )
 
         return results
