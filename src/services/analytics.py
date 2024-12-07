8f2e37dbed62267dd851af3c5887d8883d7c766c
Gabriel Casemiro <gabrielcasemiro68@gmail.com>
2024-12-07 16:46:46 -0300

    Implementing new method drug_recommendation_by_chains

--- src/services/analytics.py
+++ src/services/analytics.py
@@ -1,8 +1,9 @@
 import logging
 from ..models.claim import Claim
 from ..models.revert import Revert
+from ..models.pharmacy import Pharmacy
 from .analytics_interface import AnalyticsInterface
-from typing import List
+from typing import List, Dict
 import json
 
 
@@ -10,20 +11,24 @@ class Analytics(AnalyticsInterface):
     def __init__(self, allowed_npis) -> None:
         self.allowed_npis = allowed_npis
 
-    def compute_metrics(self, claims: List[Claim], reverts: List[Revert]):
-        results = []
-        data = {}
+    def __process_claims_and_reverts(self, claims: List[Claim], reverts: List[Revert]):
+        """
+        Process claims and reverts, returning:
+        - claims_by_id: A dictionary by claim_id with { 'key': (npi, ndc), 'price': float, 'quantity': float }
+        - data: A dictionary by (npi, ndc) with aggregated metrics -> fills, reverted, total_price, total_quantity
+        """
         claims_by_id = {}
+        data = {}
 
-        # compute claims
         for claim in claims:
-            if claim.npi not in self.allowed_npis:
-                logging.info(
-                    f"Ignored claim {claim.npi} because it's not included in the allowed npis list"
-                )
-                continue
+            if self.allowed_npis:
+                if claim.npi not in self.allowed_npis:
+                    logging.info(
+                        f"Ignored claim {claim.npi} because it's not included in the allowed npis list"
+                    )
+                    continue
 
-            if claim.id in claims_by_id.keys():
+            if claim.id in claims_by_id:
                 logging.info(f"Ignored claim {claim.npi} because it's duplicated")
                 continue
 
@@ -34,6 +39,7 @@ class Analytics(AnalyticsInterface):
                 "price": claim.price,
                 "quantity": claim.quantity,
             }
+
             if key in data.keys():
                 data[key]["fills"] += 1
                 data[key]["total_price"] += claim.price
@@ -46,8 +52,9 @@ class Analytics(AnalyticsInterface):
                     "total_quantity": claim.quantity,
                 }
 
+        # Process reverts
         for revert in reverts:
-            if revert.claim_id not in claims_by_id.keys():
+            if revert.claim_id not in claims_by_id:
                 logging.info(
                     f"Ignored revert {revert.id} because there is no valid claim_id linked to it"
                 )
@@ -58,6 +65,14 @@ class Analytics(AnalyticsInterface):
             data[claim_key]["total_quantity"] -= claim_data["quantity"]
             data[claim_key]["fills"] -= 1
             data[claim_key]["reverted"] += 1
+
+        return claims_by_id, data
+
+    def compute_metrics(self, claims: List[Claim], reverts: List[Revert]):
+        results = []
+        claims_by_id, data = self.__process_claims_and_reverts(
+            claims=claims, reverts=reverts
+        )
 
         for key_data, value in data.items():
             if value["total_quantity"] > 0:
@@ -77,3 +92,66 @@ class Analytics(AnalyticsInterface):
             )
 
         return results
+
+    def drug_recommendation_by_chains(
+        self, claims: List[Claim], reverts: List[Revert], pharmacies: List[Pharmacy]
+    ):
+
+        claims_by_id, data = self.__process_claims_and_reverts(
+            claims=claims, reverts=reverts
+        )  # data: (npi, npc) ->  fills, reverted, total_price, total_quantity
+
+        npi_to_chain = {}
+        for pharmacy in pharmacies:
+            npi_to_chain[pharmacy.npi] = pharmacy.chain
+
+        chain_data = {}  # ndc, chain -> total_price and total_quantity
+        for (npi, ndc), metrics in data.items():
+            if metrics["total_quantity"] <= 0 or npi not in npi_to_chain.keys():
+                continue
+            chain = npi_to_chain[npi]
+            key = (ndc, chain)
+            if key not in chain_data.keys():
+                chain_data[key] = {
+                    "total_price": metrics["total_price"],
+                    "total_quantity": metrics["total_quantity"],
+                }
+            else:
+                chain_data[key]["total_price"] += metrics["total_price"]
+                chain_data[key]["total_quantity"] += metrics["total_quantity"]
+
+        ndc_to_chains = {}  # ndc -> [(chain, avg_price)]
+        for (ndc, chain), value in chain_data.items():
+            avg_price = (
+                value["total_price"] / value["total_quantity"]
+                if value["total_quantity"] > 0
+                else 0.0
+            )
+            # Here we have the result, we just need to map it to a dict structure using ndc
+            if ndc not in ndc_to_chains.keys():
+                ndc_to_chains[ndc] = [(chain, avg_price)]
+            else:
+                ndc_to_chains[ndc].append((chain, avg_price))
+
+        results = []
+        for ndc, chain_info in ndc_to_chains.items():
+            chain_info.sort(key=lambda x: x[1])  # Here we order by avg_price
+            formatted_chains = []
+            for top_chain in chain_info[:2]:
+                formatted_chains.append(
+                    {"name": top_chain[0], "avg_price": round(top_chain[1], 2)}
+                )
+            results.append({"ndc": ndc, "chain": formatted_chains})
+
+        return results
+
+    def most_prescribed_quantity_by_drug(
+        self, claims: List[Claim], reverts: List[Revert], pharmacies: List[Pharmacy]
+    ):
+        """Compute drug most common quantity prescribed"""
+        return [
+            {
+                "ndc": "00002323401",
+                "most_prescribed_quantity": [8.5, 15.0, 45.0, 180.0, 2.0],
+            }
+        ]
--- src/services/analytics_interface.py
+++ src/services/analytics_interface.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from ..models.claim import Claim
 from ..models.revert import Revert
+from ..models.pharmacy import Pharmacy
 from typing import List, Dict
 
 
@@ -8,4 +9,18 @@ class AnalyticsInterface(ABC):
     @abstractmethod
     def compute_metrics(self, claims=List[Claim], reverts=List[Revert]) -> List[Dict]:
         """Compute metrics based on claims and reverts"""
+        pass
+
+    @abstractmethod
+    def drug_recommendation_by_chains(
+        self, claims: List[Claim], reverts: List[Revert], pharmacies: List[Pharmacy]
+    ) -> List[Dict]:
+        """Compute drug unit prices per Chain"""
+        pass
+
+    @abstractmethod
+    def most_prescribed_quantity_by_drug(
+        self, claims: List[Claim], reverts: List[Revert], pharmacies: List[Pharmacy]
+    ) -> List[Dict]:
+        """Compute drug most common quantity prescribed"""
         pass
